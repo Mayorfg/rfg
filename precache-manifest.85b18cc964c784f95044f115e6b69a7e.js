0ce303b4024904225a70121be6a4aa4bc7764686
Mayorfg <Akinyemimayowa54@gmail.com>
2024-02-25 19:57:38 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.85b18cc964c784f95044f115e6b69a7e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bb644613151ea0a90aba0d7f284900",
+    "url": "/rfg/index.html"
+  },
+  {
+    "revision": "3da026d6a4a5e44a897a",
+    "url": "/rfg/static/css/main.a4181176.chunk.css"
+  },
+  {
+    "revision": "d579e05d5ff251223bd9",
+    "url": "/rfg/static/js/2.660ecea6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/rfg/static/js/2.660ecea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3be20e52b6474d59d0",
+    "url": "/rfg/static/js/3.696b1a2b.chunk.js"
+  },
+  {
+    "revision": "3da026d6a4a5e44a897a",
+    "url": "/rfg/static/js/main.47b30315.chunk.js"
+  },
+  {
+    "revision": "395d2d44a5e68be1e542",
+    "url": "/rfg/static/js/runtime-main.65ed1c24.js"
+  },
+  {
+    "revision": "f8ba571c22f8e5fb6c769d986475a92c",
+    "url": "/rfg/static/media/animate.f8ba571c.gif"
+  },
+  {
+    "revision": "7c0a476d8888ebf818b277625a587f46",
+    "url": "/rfg/static/media/candle.7c0a476d.GIF"
+  },
+  {
+    "revision": "0d3b16f915e2bd5354eeb391c060f6ec",
+    "url": "/rfg/static/media/dog.0d3b16f9.JPG"
+  },
+  {
+    "revision": "406abfd91128e398ff26e36fc94fb1fb",
+    "url": "/rfg/static/media/foam.406abfd9.JPG"
+  },
+  {
+    "revision": "a42ef8a5b24cd87728a203a853517c4c",
+    "url": "/rfg/static/media/gallery3.a42ef8a5.JPG"
+  },
+  {
+    "revision": "7ed6693142aaf73ea638a80122149948",
+    "url": "/rfg/static/media/light.7ed66931.jpg"
+  },
+  {
+    "revision": "8936483f7106a711071bce6b90aaa0a8",
+    "url": "/rfg/static/media/mast.8936483f.GIF"
+  },
+  {
+    "revision": "c8cac9bc6f0c747b5c36c4d4b82e1cac",
+    "url": "/rfg/static/media/nft.c8cac9bc.jpeg"
+  },
+  {
+    "revision": "638332a48ede00d4a5745b643d2fced3",
+    "url": "/rfg/static/media/perspective.638332a4.jpeg"
+  },
+  {
+    "revision": "e0dc7efb1d7b49dc20859128179b57df",
+    "url": "/rfg/static/media/rail.e0dc7efb.jpg"
+  },
+  {
+    "revision": "e4511208e7d2c8007737d0b7b4cd36c6",
+    "url": "/rfg/static/media/rain.e4511208.GIF"
+  },
+  {
+    "revision": "d043add799e46cc9f6b09209c38760d7",
+    "url": "/rfg/static/media/shop.d043add7.JPG"
+  },
+  {
+    "revision": "653107250773540037a0964e34d52955",
+    "url": "/rfg/static/media/sunset.65310725.png"
+  },
+  {
+    "revision": "4e6ff86bb430955bb183467dbc15fd18",
+    "url": "/rfg/static/media/yard.4e6ff86b.jpeg"
+  }
+]);
